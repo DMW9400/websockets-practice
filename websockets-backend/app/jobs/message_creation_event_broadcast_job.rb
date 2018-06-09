fd108f1ddd18b3673b5f588001dcd66301a3964e
github username <github email address>
2018-06-09 13:26:02 -0400

    created broadcast classes and fixed associations

---
+++ websockets-backend/app/jobs/message_creation_event_broadcast_job.rb
@@ -0,0 +1,16 @@
+class MessageCreationEventBroadcastJob < ApplicationJob
+
+  queue as :default
+
+  def perform(message)
+    ActionCable.server.broadcast(
+      'message',
+      id: message_id,
+      created_at: message.created_at.strftime('%H:%M'),
+      username: User.find(message.user.id).username,
+      content: message.content,
+      user_id: message.user.id,
+      chatroom_id: message.chatroom.id
+    )
+  end
+end
