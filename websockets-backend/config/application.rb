3984d7fc8fdc3b0ee2d4d9fbb7f79b4ca53725db
github username <github email address>
2018-06-14 12:37:35 -0400

    configured application.rb for CORS middleware, whitelisted domain

--- websockets-backend/config/application.rb
+++ websockets-backend/config/application.rb
@@ -6,13 +6,27 @@ require 'rails/all'
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
-module WebsocketsBackend
+  module WebsocketsBackend
+    class Application < Rails::Application
+      # Initialize configuration defaults for originally generated Rails version.
+      config.load_defaults 5.1
+
+      # Settings in config/environments/* take precedence over those specified here.
+      # Application configuration should go into files in config/initializers
+      # -- all .rb files in that directory are automatically loaded.
+    end
+
+  end
+  
+module ProjectBackend
   class Application < Rails::Application
-    # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.1
 
-    # Settings in config/environments/* take precedence over those specified here.
-    # Application configuration should go into files in config/initializers
-    # -- all .rb files in that directory are automatically loaded.
+    config.middleware.insert_before 0, Rack::Cors do
+      allow do
+        origins '*'
+        resource '*', headers: :any, methods: %I[get post options]
+      end
+    end
   end
 end
